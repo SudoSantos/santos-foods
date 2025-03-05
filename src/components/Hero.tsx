e1b41c290b7348b2569ebbcc30f3a9ce0766c5c8
kanu Chukwuemeka <mekuseo@gmail.com>
2025-03-05 11:01:16 +0100

    Hero section initialized

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image';
 
 const Hero = () => {
   return (
     <div className='relative min-h-screen'>
-
+      <Image className='lg:w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 -z-10' src="/hero2.png" width={1000} height={600} alt="hero-bg" />
     </div>
   )
 };
